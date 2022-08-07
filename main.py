85474ca05a8490ae1c41403353a74af144e18f82
Avatart0dev <54482333+h65rdl0ckl3z@users.noreply.github.com>
2022-08-07 23:15:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ today = date.today().strftime('%d%m%Y')
 tomonth = date.today().strftime('%B')
 toyear = date.today().strftime('%Y')
 
-path_location = 'EGP/' + tomonth + '/' + toyear
+path_location = 'EGP/' + toyear+ '/' + tomonth
 
+file_csv = path_location + '/' + today + '.csv'
 
 def auto_egp():
 
@@ -83,7 +84,7 @@ def auto_egp():
 
             df = pd.DataFrame(list_test)
 
-            file_csv = path_location + '/' + today + '.csv'     # .csv ภาษาไทย เอ่อออ
+            # .csv ภาษาไทย เอ่อออ
 
             if os.path.isfile(file_csv) == True:
                 df.to_csv(file_csv, index=False, mode='a', header=False)
@@ -92,7 +93,68 @@ def auto_egp():
 
 
 
+def data_duplicate():
+
+    # ตัดข้อมูลซ้ำ
+    file_csv = path_location + '/' + today + '.csv'
+    df = pd.read_csv(file_csv)
+    df.drop_duplicates(subset='title', inplace=True)
+    # print(df)
+    df.to_csv(file_csv, index=False)
+
+
+
+import mysql.connector
+conn = mysql.connector.connect(user='root', password='',host='localhost', database='test')
+cursor = conn.cursor()
+
+# insert ข้อมูลที่ไม่ซ้ำ
+list_ = []
+
+def insert_():
+    sql = "SELECT title FROM egp"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for i in result:
+        # print(x)
+        for _i in i:
+            # print(_i)
+            list_.append(_i)
+
+
+def upload():
+
+    df = pd.read_csv(file_csv)
+    # print(df)
+    
+    insert_()
+    if list_ == []:
+        # print(list_)
+        for i in range(len(df['title'])):
+            print('FIRST INSERT TABLE')
+            sql = "INSERT INTO `egp` (`title`, `link`, `pubDate`, `deptID`) VALUES ('" + str(df['title'][i]) + "','" + str(df['link'][i]) + "','" + str(df['pubDate'][i]) + "','" + str(df['deptId'][i]) + "')"
+            cursor.execute(sql)
+            conn.commit()
+    else:
+        for i in range(len(df['title'])):
+            # print(i)
+            if df['title'][i] != list_[i]:
+                print('INSERT')
+                sql = "INSERT INTO `egp` (`title`, `link`, `pubDate`, `deptID`) VALUES ('" + str(df['title'][i]) + "','" + str(df['link'][i]) + "','" + str(df['pubDate'][i]) + "','" + str(df['deptId'][i]) + "')"
+                cursor.execute(sql)
+                conn.commit()
+            else:
+                print('NOT INSERT')
+    cursor.close()
+    conn.close()
+
+
+
+
 
 auto_egp()
+data_duplicate()
+upload()
+
 
 
